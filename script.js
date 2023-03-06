037b3a47f126560c31935f16922e6989acc6b677
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-06 15:30:05 -0300

    add js and style to the menu

---
+++ script.js
@@ -0,0 +1,14 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+const hamburger = document.querySelector('.nav-log');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
