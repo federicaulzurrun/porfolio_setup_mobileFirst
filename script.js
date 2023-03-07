5bbd555cf75a1c948b37e4b12f8df8398d0cfc38
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-06 21:26:46 -0300

    made changes suggested by code reviewer

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 
 const hamburger = document.querySelector('.nav-log');
 const navMenu = document.querySelector('.nav-menu');
+const containerMain = document.querySelector('.container-main');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  containerMain.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  containerMain.classList.remove('active');
 }));
