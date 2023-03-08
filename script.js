429bc73d841461f7b2b4face4629e2d0510c7385
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-08 15:26:23 -0300

    validationForm js

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
   containerMain.classList.remove('active');
 }));
+
+/* validationForm */
+
+const email = document.getElementById('email');
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+};
+
+const validateInputs = (e) => {
+  e.preventDefault();
+  const emailValue = email.value;
+  const form = document.getElementById('form');
+
+  if (emailValue === emailValue.toUpperCase()) {
+    setError(email, 'Email should be written in lower case');
+  } else {
+    form.action = ('https://formspree.io/f/mgebklpe');
+    form.submit();
+  }
+};
+
+const form = document.getElementById('form');
+form.addEventListener('click', validateInputs);
