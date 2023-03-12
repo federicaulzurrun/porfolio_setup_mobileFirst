cc13065a84ce272c9414c816ccd5c593b0953e89
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-11 22:56:29 -0300

    add changes suggested

--- script.js
+++ script.js
@@ -14,6 +14,116 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   containerMain.classList.remove('active');
 }));
 
+const firstCards = [
+  {
+    id: 0,
+    name: 'Tonic',
+    nameDesk: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    image: './img/SnapshootPortfolio.png',
+    imgDesk: './img/SnapshootPortfolio.png',
+    technologies: {
+      tech1: 'HTML',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+    },
+    counter: './img/Counter.png',
+    role: 'Back and dev',
+    company: 'canopy',
+    year: 2015,
+  }, {
+    id: 1,
+    name: 'Multi-post stories',
+    nameDesk: 'Multi-post stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    image: 'img/SnapshootPortfolio2.png',
+    imgDesk: './img/SnapshootPortfolio2.png',
+    technologies: {
+      tech1: 'HTML',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+    },
+    counter: './img/Counter.png',
+    role: 'Back and dev',
+    company: 'canopy',
+    year: 2015,
+  },
+  {
+    id: 2,
+    name: 'Tonic',
+    nameDesk: 'Facebook 360',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    image: './img/SnapshootPortfolio3.png',
+    imgDesk: './img/SnapshootPortfolio3.png',
+    technologies: {
+      tech1: 'HTML',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+    },
+    counter: './img/Counter.png',
+    role: 'Back and dev',
+    company: 'canopy',
+    year: 2015,
+  },
+  {
+    id: 3,
+    name: 'Multi-post stories',
+    nameDesk: 'Uber Navigation',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    image: './img/SnapshootPortfolio4.png',
+    imgDesk: './img/SnapshootPortfolio4.png',
+    technologies: {
+      tech1: 'HTML',
+      tech2: 'CSS',
+      tech3: 'JavaScript',
+    },
+    counter: './img/Counter.png',
+    role: 'Back and dev',
+    company: 'canopy',
+    year: 2015,
+  },
+];
+
+const cardContainer = document.getElementById('card-container-one');
+
+const renderFirstCards = (firstCards, cardContainer) => {
+  firstCards.forEach((item) => {
+    const cardLayout = document.createElement('div');
+    cardLayout.id = `card${item.id}`;
+    cardLayout.innerHTML = ` 
+    <div class="works  works-two aling-self-center m-5 d-flex  justify-content-center">
+            <img src=${item.image} class="snapshoot w-75">
+            <img src=${item.imgDesk} class="photo-one">
+            <div class="left-block">
+                <div class="primary-text-block">
+                    <h2 class="project-title">${item.name}</h2>
+                    <h2 class="project-title-desktop">${item.nameDesk}</h2>
+                    <div class="frame-2">
+                        <h6 class="client">${item.company}</h6>
+                        <img src="img/Counter.png" class="counter">
+                        <p class= "client-role">${item.role}</p>
+                        <img src="img/Counter.png" class="counter">
+                        <p class="client-year">${item.year}</p>
+                    </div>
+                </div>
+                <p class= "primary-text mx-2 my-3">A daily selection of privately personalized reads; no accounts or sign-ups required</p>
+                <ul class="tags-block mt-3">
+                    <li class="list-item"><button type="button" class= "btn btn-tags">${item.technologies.tech1}</button></li>
+                    <li class="list-item"><button type="button" class= "btn btn-tags"> ${item.technologies.tech2}</button></li>
+                    <li class="list-item"><button type="button" class= " btn btn-tags">${item.technologies.tech3}</button></li>
+                </ul>
+                <div class="action">
+                    <button type="button" class="btn normal-b" data-modal-target="#popup${item.id}" > See project</button>
+                </div>
+            </div>
+        </div>
+    `;
+    cardContainer.appendChild(cardLayout);
+  });
+};
+
+renderFirstCards(firstCards, cardContainer);
+
 const projectInfo = [
   {
     id: 0,
@@ -100,7 +210,7 @@ const projectInfo = [
     demoLink: '#',
   },
 ];
-const firstCardContainer = document.getElementById('card-container-one');
+const firstCardContainer = document.getElementById('popup-container');
 
 const renderCards = (projectInfo, containerName) => {
   projectInfo.forEach((cards) => {
@@ -109,7 +219,7 @@ const renderCards = (projectInfo, containerName) => {
     card.id = `popup${cards.id}`;
     card.innerHTML = `
     <div class="d-flex flex-column justify-content-center 
-    py-1 px-3 row-gap-3 pop-conteiner-devp" id="modal-heading-${cards.id}">
+    py-1 px-3 row-gap-3 pop-conteiner-devp mt-3 pt-5" id="modal-heading-${cards.id}">
       <div class="primary-text-block mx-2 d-flex flex-row justify-content-between w100 gap-4">
         <h2 class="project-title pt-3 fs-1">${cards.name}</h2>
         <h2 class="project-title-desktop pt-3 fs-1">${cards.nameDesk}</h2>
